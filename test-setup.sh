3917b9ef5cd23b4b2c67532755ca91b09c5b0da3
Can Berker <can@Cans-MacBook-Pro.local>
2019-04-06 13:41:12 +0200

    Run full tests.

--- test-setup.sh
+++ test-setup.sh
@@ -9,9 +9,19 @@ git co develop-dataframes
 build
 cp target/jsoniq-spark-app-0.9.5-jar-with-dependencies.jar ../performance-sparksoniq/df-jsoniq-spark-app-0.9.5-jar-with-dependencies.jar
 
+rm -f *log.txt
+rm -f *output.txt
 
+# 500k - short
+noglob spark-submit --class sparksoniq.Main --master local[*] --deploy-mode client --conf spark.executor.extraClassPath="lib/" --conf spark.driver.extraClassPath="lib/" ./jsoniq-spark-app-0.9.5-jar-with-dependencies.jar --query-path ./short500k-query1.jq --output-path ./short500k-query1-output.txt --log-path ./short500k-query1-log.txt --result-size 16000000
+noglob spark-submit --class sparksoniq.Main --master local[*] --deploy-mode client --conf spark.executor.extraClassPath="lib/" --conf spark.driver.extraClassPath="lib/" ./df-jsoniq-spark-app-0.9.5-jar-with-dependencies.jar --query-path ./short500k-query1.jq --output-path ./short500k-df-query1-output.txt --log-path ./short500k-df-query1-log.txt --result-size 16000000
 
-noglob spark-submit --class sparksoniq.Main --master local[*] --deploy-mode client --conf spark.executor.extraClassPath="lib/" --conf spark.driver.extraClassPath="lib/" ./jsoniq-spark-app-0.9.5-jar-with-dependencies.jar --query-path ./query1.jq --output-path ./query1-output.txt --log-path ./query1-log.txt --result-size 16000000
+# 5m - medium
+noglob spark-submit --class sparksoniq.Main --master local[*] --deploy-mode client --conf spark.executor.extraClassPath="lib/" --conf spark.driver.extraClassPath="lib/" ./jsoniq-spark-app-0.9.5-jar-with-dependencies.jar --query-path ./medium5m-query1.jq --output-path ./medium5m-query1-output.txt --log-path ./medium5m-query1-log.txt --result-size 16000000
+noglob spark-submit --class sparksoniq.Main --master local[*] --deploy-mode client --conf spark.executor.extraClassPath="lib/" --conf spark.driver.extraClassPath="lib/" ./df-jsoniq-spark-app-0.9.5-jar-with-dependencies.jar --query-path ./medium5m-query1.jq --output-path ./medium5m-df-query1-output.txt --log-path ./medium5m-df-query1-log.txt --result-size 16000000
 
-noglob spark-submit --class sparksoniq.Main --master local[*] --deploy-mode client --conf spark.executor.extraClassPath="lib/" --conf spark.driver.extraClassPath="lib/" ./df-jsoniq-spark-app-0.9.5-jar-with-dependencies.jar --query-path ./query1.jq --output-path ./df-query1-output.txt --log-path ./df-query1-log.txt --result-size 16000000
+# 16m - full
+noglob spark-submit --class sparksoniq.Main --master local[*] --deploy-mode client --conf spark.executor.extraClassPath="lib/" --conf spark.driver.extraClassPath="lib/" ./jsoniq-spark-app-0.9.5-jar-with-dependencies.jar --query-path ./full-query1.jq --output-path ./full-query1-output.txt --log-path ./full-query1-log.txt --result-size 16000000
+noglob spark-submit --class sparksoniq.Main --master local[*] --deploy-mode client --conf spark.executor.extraClassPath="lib/" --conf spark.driver.extraClassPath="lib/" ./df-jsoniq-spark-app-0.9.5-jar-with-dependencies.jar --query-path ./full-query1.jq --output-path ./full-df-query1-output.txt --log-path ./full-df-query1-log.txt --result-size 16000000
+
 
